2fe5b94040e1f1ac2aa73784b6eb864575506b56
Yoga <yogawahyuy@gmail.com>
2022-02-11 14:49:43 +0700

    Add Navigation Path

--- app/src/main/java/com/snystudio/resepmasakanharian/MainActivity.kt
+++ app/src/main/java/com/snystudio/resepmasakanharian/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -17,47 +19,12 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-
-    private val mOnNavigationListener=BottomNavigationView.OnNavigationItemSelectedListener{
-        when(it.itemId) {
-            R.id.home_menu -> {
-              val fragment=HomeFragment()
-                addFragment(fragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.kategori_menu -> {
-                val fragment=CategoryFragment()
-                addFragment(fragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.save_recipe_menu -> {
-                val fragment=SaveRecipeFragment()
-                addFragment(fragment)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.account_menu -> {
-                val fragment=AccountFragment()
-                addFragment(fragment)
-                return@OnNavigationItemSelectedListener true
-            }
-
-        }
-        false
-    }
-    private fun addFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
-            .replace(R.id.contents, fragment, fragment.javaClass.getSimpleName())
-            .commit()
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationListener)
-        val fragment=HomeFragment()
-        addFragment(fragment)
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.myNavHostFragment)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 
